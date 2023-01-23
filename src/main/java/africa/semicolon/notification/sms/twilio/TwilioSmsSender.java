d5129fcc6b67c6030ff746d2f39343c44076a895
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-01-23 03:10:54 +0100

    twilio pending

--- src/main/java/africa/semicolon/notification/sms/twilio/TwilioSmsSender.java
+++ src/main/java/africa/semicolon/notification/sms/twilio/TwilioSmsSender.java
@@ -3,8 +3,6 @@ package africa.semicolon.notification.sms.twilio;
 import africa.semicolon.notification.sms.SmsRequest;
 import africa.semicolon.notification.sms.SmsSender;
 import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.rest.api.v2010.account.MessageCreator;
-import com.twilio.type.PhoneNumber;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +19,13 @@ public class TwilioSmsSender implements SmsSender {
     @Override
     public void send(SmsRequest smsRequest) {
         if(isPhoneNumberValid(smsRequest.getPhoneNumber())){
-            PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNumber());
-            PhoneNumber from = new PhoneNumber(twilioConfiguration.getTrialNumber());
-            String message = smsRequest.getMessage();
-            MessageCreator creator = Message.creator(to, from, message);
-            creator.create();
-            log.info("Sent");
+            Message message = Message.creator(
+                            new com.twilio.type.PhoneNumber(smsRequest.getPhoneNumber()),
+                            new com.twilio.type.PhoneNumber(twilioConfiguration.getTrialNumber()),
+                            smsRequest.getMessage())
+                    .create();
+
+            log.info(message.getSid());
         }
     }
 
