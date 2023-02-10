3d998afaff317ecb2753c97ee778aa5a1a8bbb18
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-02-10 22:56:04 +0100

    added application-dev.properties

---
+++ src/main/java/africa/semicolon/notification/config/email/MailDevConfiguration.java
@@ -0,0 +1,43 @@
+package africa.semicolon.notification.config.maildev;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "spring.mail")
+public class MailDevConfiguration {
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("localhost");
+        mailSender.setPort(1025);
+        mailSender.setUsername("username");
+        mailSender.setPassword("password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("spring.mail.properties.mail.smtp.auth", "*");
+        props.put("spring.mail.properties.mail.smtp.ssl.trust", "true");
+        props.put("spring.mail.properties.mail.smtp.starttls.enable", "true");
+        props.put("spring.mail.properties.mail.smtp.connectiontimeout", "5000");
+        props.put("spring.mail.properties.mail.smtp.timeout", "3000");
+        props.put("spring.mail.properties.mail.smtp.writetimeout", "5000");
+
+        return mailSender;
+    }
+
+
+}
