10407f603abe2ce1abe3417cd34ec8b3682b8ee0
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-02-28 03:34:43 +0100

    security configuration 80%

--- src/main/java/africa/semicolon/notification/auth/config/JwtService.java
+++ src/main/java/africa/semicolon/notification/auth/config/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Service
 public class JwtService {
 
     private static final String SECRET_KEY = "7A244326462948404D635166546A576E5A7234753778214125442A472D4B614E";
